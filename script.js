fe412ee6060eb8ea758601f2323b46de81259c89
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-10-09 11:52:34 +0300

    Change script so it is more Class oriented

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const closeModalButton = document.querySelector(".modal-content span");
 const submitBtn = document.querySelector('button[type="submit"]');
 const warningMsg = document.querySelector(".warning");
 
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const genre = document.querySelector("#genre");
-const number = document.querySelector("#numberOfBooks");
-const isRead = document.querySelector("#readCheck");
+const titleGl = document.querySelector("#title");
+const authorGl = document.querySelector("#author");
+const genreGl = document.querySelector("#genre");
+const numberGl = document.querySelector("#numberOfBooks");
+const isReadGl = document.querySelector("#readCheck");
 
 let canClose = false;
 
@@ -29,6 +29,31 @@ class Book {
     this.numberOfBooks = numberOfBooks;
     this.hasBeenRead = hasBeenRead;
   }
+
+  addBookToLibrary() {
+    if (this.title == "" || this.genre == "" || this.author == "") {
+      warningMsg.classList.remove("hidden");
+      setTimeout(hideWarningMessage, 3000);
+      return;
+    }
+
+    library.push(this);
+
+    const markup = `
+      <tr>
+        <td>${this.title}</td>
+        <td>${this.author}</td>
+        <td>${this.genre}</td>
+        <td>${this.numberOfBooks}</td>
+        <td><span class="checkBox">${this.hasBeenRead ? "✅" : "❌"}</span></td>
+        <td><button class="delBtn">Remove</button></td>
+      </tr>
+    `;
+    booksTable.insertAdjacentHTML("beforeend", markup);
+    delBtns = document.querySelectorAll(".delBtn");
+    canClose = true;
+  }
+
   getBookSummary = function () {
     return `${title} is written by ${author}. It is a ${genre} book with ${numberOfBooks} in it's series.`;
   };
@@ -43,11 +68,11 @@ function openModal() {
 }
 
 function resetInputFields() {
-  title.value = "";
-  author.value = "";
-  genre.value = "";
-  number.value = "";
-  isRead.checked = false;
+  titleGl.value = "";
+  authorGl.value = "";
+  genreGl.value = "";
+  numberGl.value = "";
+  isReadGl.checked = false;
 }
 
 function hideWarningMessage() {
@@ -58,44 +83,10 @@ function hideNumberWarningMessage() {
   numberWarning.classList.add("hidden");
 }
 
-function AddBookToLibrary() {
-  const book = new Book(
-    title.value,
-    author.value,
-    genre.value,
-    number.value,
-    isRead.checked
-  );
+// Open modal
+addBookBtn.addEventListener("click", openModal);
 
-  if (book.title == "" || book.genre == "" || book.author == "") {
-    warningMsg.classList.remove("hidden");
-    setTimeout(hideWarningMessage, 3000);
-    return;
-  }
-
-  library.push(book);
-
-  const markup = `
-    <tr>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.genre}</td>
-      <td>${book.numberOfBooks}</td>
-      <td><span class="checkBox">${book.hasBeenRead ? "✅" : "❌"}</span></td>
-      <td><button class="delBtn">Remove</button></td>
-    </tr>
-  `;
-  booksTable.insertAdjacentHTML("beforeend", markup);
-  delBtns = document.querySelectorAll(".delBtn");
-  canClose = true;
-}
-
-// Add book
-addBookBtn.addEventListener("click", (e) => {
-  openModal();
-});
-
-// Open/close modal
+// Close modal
 closeModalButton.addEventListener("click", closeModal);
 window.addEventListener("click", (e) => {
   if (e.target == modal) closeModal();
@@ -106,19 +97,24 @@ window.addEventListener("keydown", (e) => {
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  AddBookToLibrary();
+  const book = new Book(
+    titleGl.value,
+    authorGl.value,
+    genreGl.value,
+    numberGl.value,
+    isReadGl.checked
+  );
+
+  book.addBookToLibrary();
   if (canClose) {
     hideWarningMessage();
     closeModal();
     resetInputFields();
     canClose = false;
-    delBtns.forEach((button) => {
-      button.addEventListener("click", (e) => {
-        const element = e.target.closest("td");
-        if (!element) return;
-
-        element.closest("tr").remove();
-      });
+    booksTable.addEventListener("click", (e) => {
+      const btn = e.target.closest(".delBtn");
+      if (!btn) return;
+      btn.closest("tr").remove();
     });
     canClose = false;
   }
